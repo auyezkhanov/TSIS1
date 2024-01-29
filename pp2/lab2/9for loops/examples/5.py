eaf06c4f9ee1caf2258bfdccb68bbb546bcd6c3d
auyezkhanov <156663290+auyezkhanov@users.noreply.github.com>
2024-01-29 16:21:42 +0600

    Add files via upload

---
+++ pp2/lab2/9for loops/examples/5.py
@@ -0,0 +1,15 @@
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+  
+  
+  
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+#If the loop breaks, the else block is not executed.

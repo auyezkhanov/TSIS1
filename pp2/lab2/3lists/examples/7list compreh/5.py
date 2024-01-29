da14d1efc272a490728105ea2bc0f0b7552bb3a9
auyezkhanov <156663290+auyezkhanov@users.noreply.github.com>
2024-01-29 16:18:57 +0600

    Add files via upload

---
+++ pp2/lab2/3lists/examples/7list compreh/5.py
@@ -0,0 +1,7 @@
+newlist = [x for x in range(10)]
+
+print(newlist)
+
+newlist = [x for x in range(10) if x < 5]
+
+print(newlist)
---
+++ pp2/lab2/3lists/examples/8sort lists/3.py
@@ -0,0 +1,8 @@
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+#Sort the list based on how close the number is to 50
